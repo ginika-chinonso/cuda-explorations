074268e7c066c1853079f20f5b0657fe06efc5b7
Ginika-Chinonso <ginika.chinonso.francis@gmail.com>
2025-10-25 22:43:26 +0100

    feat: init with zeros function

--- utils.h
+++ utils.h
@@ -10,6 +10,18 @@ void init_matrix(float *A, int rows, int cols) {
     }
 }
 
+// Initialize a matrix with zeros given number of rows and columns
+void init_with_zeros(float *A, int rows, int cols) {
+    // Iterate over every row
+    for (int i = 0; i < rows; ++i) {
+        // Iterate over every column
+        for (int j = 0; j < cols; ++j) {
+            // Instantiate cell value with 0
+            A[i * cols + j] = 0.0;
+        }
+    }
+}
+
 // Print a matrix given its data, number of rows and columns
 void print_matrix(float *A, int rows, int cols) {
 
